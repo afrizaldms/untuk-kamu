468bef64c017a95f8e69bb72c7a4d45200cf05c4
afrizaldms <afrizaldms96@gmail.com>
2026-07-14 00:53:43 +0700

    Implement heart rain animation feature
    Add heart animation effect to the website.

--- script.js
+++ script.js
@@ -198,3 +198,34 @@ document.getElementById("ending").classList.remove("hidden");
 // ==============================
 
 console.log("Website dibuat penuh cinta oleh Dimas ❤️");
+// =====================
+// HEART RAIN
+// =====================
+
+const heartContainer=document.getElementById("heart-container");
+
+function createHeart(){
+
+const heart=document.createElement("div");
+
+heart.classList.add("heart");
+
+heart.innerHTML="❤️";
+
+heart.style.left=Math.random()*100+"vw";
+
+heart.style.fontSize=Math.random()*20+15+"px";
+
+heart.style.animationDuration=Math.random()*4+5+"s";
+
+heartContainer.appendChild(heart);
+
+setTimeout(()=>{
+
+heart.remove();
+
+},9000);
+
+}
+
+setInterval(createHeart,500);
